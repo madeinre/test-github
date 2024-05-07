5f4d70faecb777a45e89b7ba6e92ba9c17c4a8e9
Ingin <zeleninm328@gmail.com>
2024-05-07 17:20:46 +0300

    1

---
+++ main.py
@@ -0,0 +1,9 @@
+def f(x,y):
+    if x>0 and y>0:
+        print('1')
+    elif x > 0 and y < 0:
+            print('4')
+    elif x < 0 and y > 0:
+            print('3')
+    else:
+        print("2")
